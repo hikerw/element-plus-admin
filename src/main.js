7485c1e71cddefa68e697ef10ec7313bfaa869ed
hikerw <965366906@qq.com>
2021-04-20 14:40:51 +0800

    初次提交

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import { createApp } from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import ElementPlus from 'element-plus';
-import router from './router/router.js';
+import 'dayjs/locale/zh-cn'
+import locale from 'element-plus/lib/locale/lang/zh-cn' // lang i18n
 import 'element-plus/lib/theme-chalk/index.css';
-import './assets/css/basic.css';   //公共样式
+
+import '@/styles/index.scss' // global css
+
 import App from './App.vue';
+import store from './store'
+import router from './router';
+
+import '@/icons' // icon
+import '@/permission' // permission control
+import SvgIcon from '@/components/SvgIcon'// svg component
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+ const { mockXHR } = require('../mock')
+ mockXHR()
 
 const app = createApp(App)
-app.use(ElementPlus)
+app.component('svg-icon', SvgIcon)
+app.use(ElementPlus,{ locale })
 app.use(router)
+app.use(store)
 app.mount('#app')

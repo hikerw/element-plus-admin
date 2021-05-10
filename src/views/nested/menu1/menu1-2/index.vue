7e3e4cec51483a14964d52c14f65a0bc3aca766b
hikerw <965366906@qq.com>
2021-05-10 17:20:17 +0800

    初步完成

--- src/views/nested/menu1/menu1-2/index.vue
+++ src/views/nested/menu1/menu1-2/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 1-2" type="success">
-      <router-view />
-    </el-alert>
+    
+	<router-view v-slot="{ Component }">
+	<el-alert :closable="false" title="menu 1-2" type="success">
+	    <component :is="Component" />
+	 </el-alert>
+	</router-view>
   </div>
 </template>
